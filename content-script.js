f5fb5e97b63e6056170d57bc316b26e4233a56f5
killzoms <killzoms@users.noreply.github.com>
2017-11-17 09:51:31 -0500

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -1,1 +1,4 @@
-//placeholder
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+if (tabs.url == "https://learner.ple.platoweb.com/secondary/lessonportfolio") {
+  chrome.tabs.connect(tabs[0].id, {name: "Information"});
+}
