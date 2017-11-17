df022b09babe847330870e61923ba3a748433d67
killzoms <killzoms@users.noreply.github.com>
2017-11-17 09:48:56 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,7 @@
 //placeholder
+chrome.runtime.onConnect.addListener(function(port) {
+  console.assert(port.name == "Information");
+  port.onMessage.addListener(function(msg) {
+    chrome.storage.local.set(msg);
+  });
+});
