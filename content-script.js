7fbc69f6f6d7089550e73e9d4f55000933dcee4c
killzoms <killzoms@users.noreply.github.com>
2017-11-17 10:24:22 -0500

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -1,4 +1,21 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-if (tabs.url == "https://learner.ple.platoweb.com/secondary/lessonportfolio") {
-  chrome.tabs.connect(tabs[0].id, {name: "Information"});
+if (document.url == "https://learner.ple.platoweb.com/secondary/lessonportfolio") {
+  var port = chrome.runtime.connect({name: "Information"});
+} elseif (document.url == "https://learner.ple.platoweb.com/secondary/messages") {
+  var eArea = document.getElementsByClassName("k-content");
+  var i;
+  for (i in eArea) {
+    if (eArea[i].title == "Editable area. Press F10 for toolbar.") {
+      chrome.storage.local.get({"Progress", "ClassName"}, function(progress, className) {
+        textFrame = eArea[i].contentDocument.body;
+        for (v in className) {
+          if (content != null) {
+            var content += "<tr><td>" + className[v] + "</td><td>" + progress[v] + "</td></tr>";
+          } else {
+            var content = "<tr><td>" + className[v] + "</td><td>" + progress[v] + "</td></tr>";
+          }
+        }
+        textFrame.innerHTML = "<table><tbody><tr><td>Class Name</td><td>Progress</td></tr>"+ content +"</tbody></table>;
+      }
+    }
+  }
 }
