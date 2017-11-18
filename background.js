8c2e86e3306069f6c4134c9f98c05d8069b0b095
killzoms <killzoms@users.noreply.github.com>
2017-11-17 20:09:57 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 chrome.runtime.onConnect.addListener(function(port) {
+  console.log("Connected")
   console.assert(port.name == "Information");
   port.onMessage.addListener(function(msg) {
     chrome.storage.local.set(msg);
