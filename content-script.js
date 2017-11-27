617bc1e3f1c883432418cdcc61b5e76df3291556
killzoms <killzoms@users.noreply.github.com>
2017-11-27 10:48:44 -0500

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -1,37 +1,5 @@
 var i;
 
-function progressReportList() {
-  var tabsReplyContainer = document.getElementsByClassName("tabsReplyContainer")[0];
-  tabsReplyContainer.style.width = "402px";
-  var MessageContainer = document.getElementsByClassName("messageDetailsArea")[0];
-  MessageContainer.style.left = "424.4px";
-  var LastTab = document.getElementsByClassName("archive k-item k-last k-state-default")[0];
-  var PRTab = LastTab.cloneNode(true);
-  LastTab.className = "archive k-item k-state-default";
-  PRTab.firstChild.nextSibling.firstChild.innerHTML = "Progress Reports";
-  PRTab.className = "progress-report k-item k-last k-state-default";
-  var curAriaCtrls = PRTab.getAttribute("aria-controls");
-  var newAriaCtrls = curAriaCtrls.slice(0, -1) + "4";
-  PRTab.setAttribute("aria-controls", newAriaCtrls);
-  var tabsList = document.getElementsByClassName("k-tabstrip-items k-reset")[0];
-  tabsList.appendChild(PRTab);
-  var ArchTab = document.getElementsByClassName("archiveTab k-content")[0];
-  var PRInfo = ArchTab.cloneNode(true);
-  PRInfo.className = "progress-reportTab k-content";
-  var curAriaCtrls = PRInfo.getAttribute("id");
-  var newAriaCtrls = curAriaCtrls.slice(0, -1) + "4";
-  PRInfo.setAttribute("id", newAriaCtrls);
-  var tabsList = document.getElementsByClassName("tabs k-tabstrip k-widget k-header k-floatwrap k-tabstrip-top")[0];
-  var msgList = document.getElementsByClassName("messagesList k-widget k-listview")[0];
-  var msgListLength = msgList.children.length;
-  var msgListChildren = msgList.children;
-  for (var i = 0; i < msgListLength; i++) {
-    console.log(msgListChildren[i]);
-  }
-  tabsList.appendChild(PRInfo);
-  
-}
-
 if (location.href == "https://learner.ple.platoweb.com/secondary/lessonportfolio") {
   setTimeout(function() {
   var classNames = [];
@@ -121,5 +89,32 @@ if (location.href == "https://learner.ple.platoweb.com/secondary/lessonportfolio
       });
     }, 500);
   });
-  progressReportList();
+  var tabsReplyContainer = document.getElementsByClassName("tabsReplyContainer")[0];
+  tabsReplyContainer.style.width = "402px";
+  var MessageContainer = document.getElementsByClassName("messageDetailsArea")[0];
+  MessageContainer.style.left = "424.4px";
+  var LastTab = document.getElementsByClassName("archive k-item k-last k-state-default")[0];
+  var PRTab = LastTab.cloneNode(true);
+  LastTab.className = "archive k-item k-state-default";
+  PRTab.firstChild.nextSibling.firstChild.innerHTML = "Progress Reports";
+  PRTab.className = "progress-report k-item k-last k-state-default";
+  var curAriaCtrls = PRTab.getAttribute("aria-controls");
+  var newAriaCtrls = curAriaCtrls.slice(0, -1) + "4";
+  PRTab.setAttribute("aria-controls", newAriaCtrls);
+  var tabsList = document.getElementsByClassName("k-tabstrip-items k-reset")[0];
+  tabsList.appendChild(PRTab);
+  var ArchTab = document.getElementsByClassName("archiveTab k-content")[0];
+  var PRInfo = ArchTab.cloneNode(true);
+  PRInfo.className = "progress-reportTab k-content";
+  var curAriaCtrls = PRInfo.getAttribute("id");
+  var newAriaCtrls = curAriaCtrls.slice(0, -1) + "4";
+  PRInfo.setAttribute("id", newAriaCtrls);
+  var tabsList = document.getElementsByClassName("tabs k-tabstrip k-widget k-header k-floatwrap k-tabstrip-top")[0];
+  var msgList = document.getElementsByClassName("messagesList k-widget k-listview")[0];
+  var msgListLength = msgList.children.length;
+  var msgListChildren = msgList.children;
+  for (var i = 0; i < msgListLength; i++) {
+    console.log(msgListChildren[i]);
+  }
+  tabsList.appendChild(PRInfo);
 };
